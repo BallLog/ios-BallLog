38ff5fd6c32a23f5be4413cb37607ae3aaa7563e
haileyport <sugar9935@gmail.com>
2024-10-17 21:33:54 +0900

    Initial Commit

---
+++ balllog/balllogApp.swift
@@ -0,0 +1,17 @@
+//
+//  balllogApp.swift
+//  balllog
+//
+//  Created by 전은혜 on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct balllogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
