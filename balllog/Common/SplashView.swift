78952032406f4fe52678aa1cde49510869882a7d
haileyport <sugar9935@gmail.com>
2024-10-17 21:40:50 +0900

    init: 프로젝트 생성
    스플래시 화면 적용

---
+++ balllog/Common/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  balllog
+//
+//  Created by 전은혜 on 10/17/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text("짜잔 나는 볼로그!")
+    }
+}
+
+#Preview {
+    SplashView()
+}
--- balllog/ContentView.swift
+++ balllog/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showMainView = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if showMainView {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            
+        } else {
+            SplashView()
+                .onAppear{
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        withAnimation {
+                            showMainView = true
+                        }
+                    }
+                }
         }
-        .padding()
     }
 }
 
